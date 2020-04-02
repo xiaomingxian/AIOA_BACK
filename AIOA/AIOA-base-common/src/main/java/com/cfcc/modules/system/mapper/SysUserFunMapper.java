8e2af7f925c90ff2ef3bfb0d892ea248301506bb
xubin1024 <934792834@qq.com>
2020-04-02 11:12:49 +0800

    业务收藏状态显示

--- AIOA/AIOA-base-common/src/main/java/com/cfcc/modules/system/mapper/SysUserFunMapper.java
+++ AIOA/AIOA-base-common/src/main/java/com/cfcc/modules/system/mapper/SysUserFunMapper.java
@@ -26,6 +26,12 @@ public interface SysUserFunMapper extends BaseMapper<SysUserFun> {
     @Update("UPDATE sys_user_fun set status=#{statuss} WHERE s_user_id=#{userId} and i_bus_function_id=#{functionId};")
     void updateByUserId(String userId, Integer functionId, Integer statuss);
 
-    @Select("SELECT b.s_name FROM  sys_user_fun u LEFT join oa_bus_function b on u.i_bus_function_id=b.i_id WHERE u.s_user_id=#{userId} and u.status !=0")
+    @Select("SELECT b.s_name FROM  sys_user_fun u " +
+            "LEFT join oa_bus_function b " +
+            "on u.i_bus_function_id=b.i_id and u.i_bus_model_id=b.i_bus_model_id " +
+            "WHERE u.s_user_id=#{userId} and u.status !=0")
     List<String> selecUserFuntList(String userId);
+
+    @Select("SELECT i_bus_model_id,i_bus_function_id,status FROM sys_user_fun WHERE s_user_id=#{usrId}")
+    List<SysUserFun> selectUserFunStatus(String userId);
 }
