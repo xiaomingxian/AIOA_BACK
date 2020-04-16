8ceb3796c9eea631213cd34ba5720970d51c0fb3
xubin1024 <934792834@qq.com>
2020-04-16 09:57:08 +0800

    明细实体类调整

---
+++ AIOA/AIOA-business-common/src/main/java/com/cfcc/modules/oadatafetailedinst/mapper/OaDatadetailedInstMapper.java
@@ -0,0 +1,17 @@
+package com.cfcc.modules.oadatafetailedinst.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import com.cfcc.modules.oadatafetailedinst.entity.OaDatadetailedInst;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 明细存储
+ * @Author: jeecg-boot
+ * @Date:   2020-04-16
+ * @Version: V1.0
+ */
+public interface OaDatadetailedInstMapper extends BaseMapper<OaDatadetailedInst> {
+
+}
---
+++ AIOA/AIOA-business-common/src/main/java/com/cfcc/modules/oadatafetailedinst/service/IOaDatadetailedInstService.java
@@ -0,0 +1,14 @@
+package com.cfcc.modules.oadatafetailedinst.service;
+
+import com.cfcc.modules.oadatafetailedinst.entity.OaDatadetailedInst;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 明细存储
+ * @Author: jeecg-boot
+ * @Date:   2020-04-16
+ * @Version: V1.0
+ */
+public interface IOaDatadetailedInstService extends IService<OaDatadetailedInst> {
+
+}
