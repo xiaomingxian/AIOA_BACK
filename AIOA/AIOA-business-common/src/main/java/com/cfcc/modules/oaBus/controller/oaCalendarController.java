1ac65fc2f29af8f5001c02fa86bf7a74d8e5eb38
Liuyujing <liuyujing0411@163.com>
2020-04-17 09:22:16 +0800

    修改

--- AIOA/AIOA-business-common/src/main/java/com/cfcc/modules/oaBus/controller/oaCalendarController.java
+++ AIOA/AIOA-business-common/src/main/java/com/cfcc/modules/oaBus/controller/oaCalendarController.java
@@ -335,9 +335,6 @@ public class oaCalendarController implements Job {
         String token = request.getHeader(DefContants.X_ACCESS_TOKEN);
         String username = JwtUtil.getUsername(token);
         try {
-            Integer busDataId = oaCalendar.getIFunDataId();
-            oaCalendar o = oaCalendarService.findBybusDataId(busDataId);
-            if(o == null){
                 // 给定截取条件分割字符串为字符数组
                 String[] split = oaCalendar.getSUserNames().split(",");
                 List<SysUser> listUsr = new ArrayList<>();
@@ -376,15 +373,6 @@ public class oaCalendarController implements Job {
 
                 oaCalendarService.saveCalendar(oaCalendar);
                 result.success("添加成功！");
-            }else{
-                String sUserNames = oaCalendar.getSUserNames();
-                String sUserNames1 = o.getSUserNames();
-                String s = sUserNames + sUserNames1 + ",";
-                oaCalendar.setSUserNames(s);
-                oaCalendar.setIId(o.getIId());
-                oaCalendarService.updateByIid(oaCalendar);
-            }
-
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             result.error500("操作失败");
@@ -459,10 +447,37 @@ public class oaCalendarController implements Job {
                 oaCalendarService.saveCalendar(oaCalendar);
                 result.success("添加成功！");
             }else{
-                String sUserNames = oaCalendar.getSUserNames();
+                // 给定截取条件分割字符串为字符数组
+                String[] split = oaCalendar.getSUserNames().split(",");
+                List<SysUser> listUsr = new ArrayList<>();
                 String sUserNames1 = o.getSUserNames();
-                String s = sUserNames + sUserNames1 + ",";
-                oaCalendar.setSUserNames(s);
+                String[] split1 = sUserNames1.split(",");
+                //接收排序好的字符串
+                String str = "";
+                for (int i = 0; i < split.length; i++) {
+                    if(!sUserNames1.contains(split[i])){
+                        SysUser user = sysUserService.getUserByName(split[i]); //根据username查出user
+                        if (user != null) {
+                            listUsr.add(user);
+                        }
+                    }
+
+                }
+                for (int i = 0; i < split1.length; i++) {
+                        SysUser user = sysUserService.getUserByName(split1[i]); //根据username查出user
+                        if (user != null) {
+                            listUsr.add(user);
+                        }
+                }
+
+                List<SysUser> listUserOrder = listUsr.stream().sorted(Comparator.comparing(SysUser::getShowOrder)).collect(Collectors.toList());
+                //遍历show_order   根据show_order 找到对应的id
+
+                for (SysUser sysUser : listUserOrder) {
+                    str = str + sysUser.getUsername() + ",";
+                }
+                oaCalendar.setSUserNames(str);
+
                 oaCalendar.setIId(o.getIId());
                 oaCalendarService.updateByIid(oaCalendar);
             }
