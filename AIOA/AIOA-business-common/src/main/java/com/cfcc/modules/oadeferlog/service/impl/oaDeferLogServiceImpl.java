ff63736e3086b5ee635793b0cf391e8d41c0775c
xubin1024 <934792834@qq.com>
2020-04-24 11:10:43 +0800

    督办延期

--- AIOA/AIOA-business-common/src/main/java/com/cfcc/modules/oadeferlog/service/impl/oaDeferLogServiceImpl.java
+++ AIOA/AIOA-business-common/src/main/java/com/cfcc/modules/oadeferlog/service/impl/oaDeferLogServiceImpl.java
@@ -1,11 +1,17 @@
 package com.cfcc.modules.oadeferlog.service.impl;
 
+import com.cfcc.modules.oaBus.mapper.OaBusDynamicTableMapper;
 import com.cfcc.modules.oadeferlog.entity.oaDeferLog;
 import com.cfcc.modules.oadeferlog.mapper.oaDeferLogMapper;
 import com.cfcc.modules.oadeferlog.service.IoaDeferLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description: 任务类事务延期记录
@@ -16,4 +22,30 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 @Service
 public class oaDeferLogServiceImpl extends ServiceImpl<oaDeferLogMapper, oaDeferLog> implements IoaDeferLogService {
 
+    @Autowired
+    private OaBusDynamicTableMapper dynamicTableMapper;
+
+    @Override
+    public boolean updataOadataAndaddOaDeferLog(Map<String, Object> map) {
+        Map<String, Object> mapdata = new HashMap<>();
+        mapdata.put("table",map.get("s_table"));
+        mapdata.put("i_id",map.get("i_id"));
+        mapdata.put("d_datetime2",map.get("d_datetime2"));
+        int num=dynamicTableMapper.updateData(mapdata);
+        boolean log=true;
+        if (num!=0){
+            map.put("table","oa_defer_log");
+            map.put("i_table_id",map.get("i_id"));
+            Date date=new Date();
+            map.put("d_create_time",date);
+            map.remove("i_id");
+            int flag=dynamicTableMapper.insertData(map);
+            if (flag == 0){
+                log=false;
+            }
+        }else {
+            log=false;
+        }
+        return log;
+    }
 }
